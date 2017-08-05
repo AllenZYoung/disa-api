ef805f956f8457b12affbfda10aa386500987200
Cole Hansen <cole_hansen@brown.edu>
2017-08-05 14:40:24 -0400

    Fix permissions to internal entries

--- entries.js
+++ entries.js
@@ -139,7 +139,8 @@ function getEntry(req, resp, id) {
           }
           code = 401;
         } else {
-          if ("" + result.meta.creator !== user.id) {
+          if (results.meta.stage === "Draft" && 
+              "" + result.meta.creator !== user.id) {
             response = {
               error: "You do not have access to someone else's drafts."
             }
